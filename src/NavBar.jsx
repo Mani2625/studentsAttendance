0129366fe72cea871ac844ebf3a9c715cfd69664
Mani2625 <manibalan2625@gmail.com>
2025-07-29 21:12:43 +0530

    Mobile UI

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleLinkClick = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <div className="navbar">
-            <ul>
+            <button className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                &#9776; {/* Hamburger icon */}
+            </button>
+            <ul className={isMenuOpen ? 'open' : ''}>
                 <li>
                     <NavLink to="/" end
                         className={({ isActive }) => isActive ? 'active' : ''}
+                        onClick={handleLinkClick}
                     >
                         Home
                     </NavLink>
@@ -14,6 +25,7 @@ function NavBar() {
                 <li>
                     <NavLink to="/mark-attendance"
                         className={({ isActive }) => isActive ? 'active' : ''}
+                        onClick={handleLinkClick}
                     >
                         Mark Attendance
                     </NavLink>
@@ -21,6 +33,7 @@ function NavBar() {
                 <li>
                     <NavLink to="/sra"
                         className={({ isActive }) => isActive ? 'active' : ''}
+                        onClick={handleLinkClick}
                     >
                         SRA
                     </NavLink>
@@ -28,6 +41,7 @@ function NavBar() {
                 <li>
                     <NavLink to="/warden"
                         className={({ isActive }) => isActive ? 'active' : ''}
+                        onClick={handleLinkClick}
                     >
                         Warden
                     </NavLink>
@@ -35,6 +49,7 @@ function NavBar() {
                 <li>
                     <NavLink to="/add-student"
                         className={({ isActive }) => isActive ? 'active' : ''}
+                        onClick={handleLinkClick}
                     >
                         Add Student
                     </NavLink>
